ab3ef7d02fcab1384cd97e64bac510aa5fb0d264
HubashovD <34233868+HubashovD@users.noreply.github.com>
2022-08-11 19:47:57 +0300

    Update retrieve_data_Rev2.js

--- scripts/retrieve_data_Rev2.js
+++ scripts/retrieve_data_Rev2.js
@@ -790,152 +790,183 @@ function retrieve_data(value) {
 
             }
 
+
+            var bar_margin = { top: 10, right: 50, bottom: 10, left: 100 },
+                bar_width = d3.select("#barBlock").node().getBoundingClientRect().width - bar_margin.left - bar_margin.right,
+                bar_height = d3.select("#barBlock").node().getBoundingClientRect().height - bar_margin.top - bar_margin.bottom;
+
+            var bar_svg = d3.select("#barBlock")
+                .append("svg")
+                .attr("width", bar_width + bar_margin.left + bar_margin.right)
+                .attr("height", bar_height + bar_margin.top + bar_margin.bottom)
+                .append("g")
+                .attr("transform",
+                    "translate(" + bar_margin.left + "," + bar_margin.top + ")");
+
+            var bar_y = d3.scaleBand()
+                .range([0, bar_height])
+                .padding(0.2);
+
+            var bar_xAxis = bar_svg.append("g")
+                .attr("transform", "translate(0," + bar_height + ")")
+
+            // Initialize the Y axis
+            var bar_x = d3.scaleLinear()
+                .range([0, bar_width])
+
+
+            var bar_yAxis = bar_svg.append("g")
+                .attr("class", "myYaxis")
+
+            bar_yAxis.call(d3.axisLeft(bar_y))
+
             function drawBar(data) {
-                // set the dimensions and margins of the graph
-                var bar_margin = { top: 10, right: 50, bottom: 10, left: 100 },
-                    bar_width = d3.select("#barBlock").node().getBoundingClientRect().width - bar_margin.left - bar_margin.right,
-                    bar_height = d3.select("#barBlock").node().getBoundingClientRect().height - bar_margin.top - bar_margin.bottom;
-
-                // append the svg object to the body of the page
-                var bar_svg = d3.select("#barBlock")
-                    .append("svg")
-                    .attr("width", bar_width + bar_margin.left + bar_margin.right)
-                    .attr("height", bar_height + bar_margin.top + bar_margin.bottom)
-                    .append("g")
-                    .attr("transform",
-                        "translate(" + bar_margin.left + "," + bar_margin.top + ")");
-
-                // Initialize the X axis
-                var bar_y = d3.scaleBand()
-                    .range([0, bar_height])
-                    .padding(0.2);
-
-                var bar_xAxis = bar_svg.append("g")
-                    .attr("transform", "translate(0," + bar_height + ")")
-
-                // Initialize the Y axis
-                var bar_x = d3.scaleLinear()
-                    .range([0, bar_width])
 
 
-                var bar_yAxis = bar_svg.append("g")
-                    .attr("class", "myYaxis")
+                var parent = document.querySelector('#yearsSelector')
+                console.log(parent)
+                const btn = parent.querySelector('.select__toggle')
+                year = btn.value
+
+                countries_selected = []
+                countries.forEach(function(d) {
+                    var cbb = document.querySelector('#' + d.id);
+                    if (cbb.checked == true) {
+                        countries_selected.push(cbb.value)
+                            // console.log(countries_selected)
+                    } else {
+                        countries_selected.filter((n) => { return n != cbb.value.toString() })
+                    }
+                })
+
+                console.log(countries_selected)
+
+
+                var filteredData = data.filter(function(elem) {
+                    // console.log(elem)
+                    if (countries_selected.includes(elem.countryiso3code)) {
+                        return elem
+                    } else {}
+                })
+
+                filteredData = filteredData.filter(function(elem) {
+                    if (elem.date === year) {
+                        return elem
+                    }
+                })
+
+                data = filteredData
+
+                console.log(data)
+
+                data.sort(function(b, a) {
+                    return a.value - b.value;
+                });
+
+                var arr = []
+                data.forEach(function(d) {
+                    arr.push(+d.value)
+                })
+
+                function domainRes(arr) {
+                    if (d3.min(arr) > 0) {
+                        // console.log(d3.min(arr))
+                        return [0, d3.max(arr)]
+                    } else {
+                        return [d3.min(arr), d3.max(arr)]
+                    }
+                }
+
+                domainResult = domainRes(arr)
+                    // console.log(domainResult)
+
+
+                // console.log(data)
+
+                // Update the X axis
+                bar_y
+                    .domain(data.map(function(d) {
+                        return d.country.value;
+                    }))
 
                 bar_yAxis.call(d3.axisLeft(bar_y))
 
 
-                // A function that create / update the plot for a given variable:
-                function bar_update(data) {
-                    data.sort(function(b, a) {
-                        return a.value - b.value;
+                // Update the Y axis
+                bar_x.domain(domainResult)
+                bar_xAxis.transition().duration(1).call(d3.axisBottom(bar_x));
+
+                bar_xAxis.attr('class', 'bottomAxis')
+
+                // console.log(bar_xAxis)
+
+
+                // Create the u variable
+                var bar_u = bar_svg.selectAll("rect")
+                    .data(data)
+
+                bar_u
+                    .enter()
+                    .append("rect") // Add a new rect for each new elements
+                    .merge(bar_u) // get the already existing elements as well
+                    .transition() // and apply changes to all of them
+                    .duration(1)
+                    // .attr("x", 0)
+                    .attr("x", function(d) {
+                        // console.log(bar_x(Math.min(0, d.value)))
+                        return bar_x(Math.min(0, d.value));
+                    })
+                    .attr("y", function(d) { return bar_y(d.country.value); })
+                    .attr("height", bar_y.bandwidth())
+                    // .attr("width", function(d) { return bar_x(d.value); })
+                    .attr("width", function(d) { return Math.abs(bar_x(d.value) - bar_x(0)); })
+                    .attr("fill", "#a7cc33")
+
+
+
+                // If less group in the new dataset, I delete the ones not in use anymore
+
+                bar_u
+
+                    .exit()
+
+                .remove()
+
+
+
+                var labels = bar_svg.selectAll(".label")
+                    .data(data)
+
+                // console.log(labels)
+                var f = d3.format(".2s")
+                labels
+                    .enter()
+                    .append("text")
+                    .attr("class", "label")
+                    .merge(labels)
+                    .attr("y", function(d) { return bar_y(d.country.value); })
+                    .transition() // and apply changes to all of them
+                    .duration(10)
+                    .attr("x", (function(d) { return bar_x(d.value); }))
+                    .attr("y", function(d) { return bar_y(d.country.value) + (bar_y.bandwidth() / 2) + 3; })
+                    .attr("dy", ".75em")
+                    .text(function(d) {
+                        // console.log(d)
+                        if (d.value < 1.0) {
+                            return d.value
+                        } else {
+                            return f(d.value)
+                        }
                     });
 
-                    var arr = []
-                    data.forEach(function(d) {
-                        arr.push(+d.value)
-                    })
-
-                    function domainRes(arr) {
-                        if (d3.min(arr) > 0) {
-                            // console.log(d3.min(arr))
-                            return [0, d3.max(arr)]
-                        } else {
-                            return [d3.min(arr), d3.max(arr)]
-                        }
-                    }
-
-                    domainResult = domainRes(arr)
-                        // console.log(domainResult)
-
-
-                    // console.log(data)
-
-                    // Update the X axis
-                    bar_y
-                        .domain(data.map(function(d) {
-                            return d.country;
-                        }))
-
-                    bar_yAxis.call(d3.axisLeft(bar_y))
-
-
-                    // Update the Y axis
-                    bar_x.domain(domainResult)
-                    bar_xAxis.transition().duration(1).call(d3.axisBottom(bar_x));
-
-                    bar_xAxis.attr('class', 'bottomAxis')
-
-                    // console.log(bar_xAxis)
-
-
-                    // Create the u variable
-                    var bar_u = bar_svg.selectAll("rect")
-                        .data(data)
-
-                    bar_u
-                        .enter()
-                        .append("rect") // Add a new rect for each new elements
-                        .merge(bar_u) // get the already existing elements as well
-                        .transition() // and apply changes to all of them
-                        .duration(1)
-                        // .attr("x", 0)
-                        .attr("x", function(d) {
-                            // console.log(bar_x(Math.min(0, d.value)))
-                            return bar_x(Math.min(0, d.value));
-                        })
-                        .attr("y", function(d) { return bar_y(d.country); })
-                        .attr("height", bar_y.bandwidth())
-                        // .attr("width", function(d) { return bar_x(d.value); })
-                        .attr("width", function(d) { return Math.abs(bar_x(d.value) - bar_x(0)); })
-                        .attr("fill", "#a7cc33")
-
-
-
-                    // If less group in the new dataset, I delete the ones not in use anymore
-
-                    bar_u
-
-                        .exit()
-
+                labels
+                    .exit()
                     .remove()
 
 
 
-                    var labels = bar_svg.selectAll(".label")
-                        .data(data)
-
-                    // console.log(labels)
-                    var f = d3.format(".2s")
-                    labels
-                        .enter()
-                        .append("text")
-                        .attr("class", "label")
-                        .merge(labels)
-                        .attr("y", function(d) { return bar_y(d.country); })
-                        .transition() // and apply changes to all of them
-                        .duration(10)
-                        .attr("x", (function(d) { return bar_x(d.value); }))
-                        .attr("y", function(d) { return bar_y(d.country) + (bar_y.bandwidth() / 2) + 3; })
-                        .attr("dy", ".75em")
-                        .text(function(d) {
-                            // console.log(d)
-                            if (d.value < 1.0) {
-                                return d.value
-                            } else {
-                                return f(d.value)
-                            }
-                        });
-
-                    labels
-                        .exit()
-                        .remove()
-
-
-                }
-
-                // Initialize the plot with the first dataset
-                // bar_update()
             }
+            drawBar(cleanData)
 
             function drawScatter(data) {}
 
@@ -943,28 +974,28 @@ function retrieve_data(value) {
                 const btn = e.target.querySelector('.select__toggle');
                 console.log(`Выбранное значение: ${btn.value}`);
                 drawMap(cleanData)
-                    // drawBar(cleanData)
+                drawBar(cleanData)
             });
 
             var deSelectAllButton = document.querySelector('#deSelectAllButton')
             deSelectAllButton.addEventListener('click', (event) => {
                 filterData(cleanData)
                 drawMap(cleanData)
-                    // drawBar(cleanData)
+                drawBar(cleanData)
             })
 
             var selectAllButton = document.querySelector('#selectAllButton')
             selectAllButton.addEventListener('click', (event) => {
                 filterData(cleanData)
                 drawMap(cleanData)
-                    // drawBar(cleanData)
+                drawBar(cleanData)
             })
 
             var menaSelectButton = document.querySelector('#menaSelectButton')
             menaSelectButton.addEventListener('click', (event) => {
                 filterData(cleanData)
                 drawMap(cleanData)
-                    // drawBar(cleanData)
+                drawBar(cleanData)
             })
 
 
